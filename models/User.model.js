b4e47e073935d2f04d23252263e02268b3e999a3
nagyrobert92 <nagyrobert1992@yahoo.com>
2021-05-13 22:34:22 +0200

    updated User model with more fields

--- models/User.model.js
+++ models/User.model.js
@@ -2,16 +2,35 @@ const { Schema, model } = require("mongoose");
 
 // 1. Define your schema
 let UserSchema = new Schema({
-  name: String, 
+  name: {
+    type: String,
+    require: true,
+    min: 5,
+    max: 25,
+    unique: true
+  },
   email: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
   passwordHash: {
     type: String,
     required: true
+  },
+  profilePicture: {
+    type: String,
+    default: "",
+  },
+  followers: {
+    type: Array,
+    default: [],
+  },
+  follows: {
+    type: Array,
+    default: [],
   }
-})
+}, { timestamps: true })
 
 // 2. Define your model
 let UserModel = model('user', UserSchema)
