69db1a79ace29fe6fcc4311ce7ba0478ca1c2c9e
nagyrobert92 <nagyrobert1992@yahoo.com>
2021-05-20 20:21:07 +0200

    user route updated for fetching  individual user and its posts

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,21 +1,26 @@
 const express = require('express')
 const router = express.Router()
 const UserModel = require('../models/User.model');
+const PostModel = require("../models/Post.model")
 const isLoggedIn = require('../middleware/isLoggedIn')
 
 //get individual user
-router.get("/user/:id", isLoggedIn, (req, res) => {
-  UserModel.findById(req.params.id)
-  .then((res) => {
-       res.status(200).json(req.session.loggedInUser)
-  })
-  .catch((err) => {
-       res.status(500).json({
-            error: 'Something went wrong',
-            message: err
-       })
-  })  
-});
+router.get('/user/:id',isLoggedIn,(req,res)=>{
+    UserModel.findOne({_id:req.params.id})  //Get User
+    .select("-password") 
+    .then(user=>{
+         PostModel.find({postedBy:req.params.id}) //Get users posts
+         .populate("postedBy","_id name")
+         .exec((err,posts)=>{
+             if(err){
+                 return res.status(422).json({error:err})
+             }
+             res.json({user,posts})
+         })
+    }).catch(err=>{
+        return res.status(404).json({error: err})
+    })
+})
 
 
 
